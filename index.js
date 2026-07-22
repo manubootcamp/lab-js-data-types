85601a3f3ea33a40b3ead40156a0f1a051626aac
manult20 <emmanuel-lt@proton.me>
2026-07-22 11:54:59 +0200

    exercice done

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const concatenated_string = s1 + " " + s2 + " " + s3  + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " "+ s1 + " "+ s4 
 
 // Print out the concatenated string
 
-
+console.log(concatenated_string)
 
 
 /*******************************************
@@ -22,8 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+const part1_uppercase = part1.slice(0, part1.length - 1) + part1[part1.length - 1].toUpperCase();
+const part2_uppercase = part2.slice(0, part2.length - 1) + part2[part2.length - 1].toUpperCase()
+console.log(part1_uppercase)
+console.log(part2_uppercase)
+const result = part1_uppercase + part2_uppercase
+console.log(result)
 // Print the cameLtaiL-formatted string
 
 
@@ -35,10 +39,11 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
+const tipAmount = 84 * 0.15
 
 
 // Print out the tipAmount
-
+console.log(tipAmount)
 
 
 
@@ -47,11 +52,11 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const randomNumber = Math.floor(Math.random()*10)+1
 
 // Print the generated random number
 
-
+console.log(randomNumber)
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -62,15 +67,16 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
-
+//false
 const expression2 = a || b;
-
+//true
 const expression3 = !a && b;
-
+//false
 const expression4 = !(a && b);
-
+//true
 const expression5 = !a || !b;
-
+//true
 const expression6 = !(a || b);
-
+//false
 const expression7 = a && a;
+//true
